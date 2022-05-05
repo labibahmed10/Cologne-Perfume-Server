0b679eee6b6ffc830e5653ba461b563093b26232
labibahmed10 <labib.ahmed.372@gmail.com>
2022-05-05 20:59:16 +0600

    created jwt token for for signup,login,social,sigin and added secret files in heroku

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+//json web token
+const jwt = require("jsonwebtoken");
+
 //mongodb side
 
 const uri = `mongodb+srv://${process.env.COL_USER}:${process.env.COL_PASS}@cluster0.zqp7w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -37,18 +40,24 @@ async function run() {
 
     //loading inventory items
     app.get("/inventory", async (req, res) => {
-      const query = {};
-      const cursor = productImage.find(query);
-      const result = await cursor.toArray();
-      res.send(result);
+      const pageNum = req.query.pageNum;
+      const size = req.query.size;
+
+      const cursor = productImage.find({});
+      const result = await cursor
+        .skip(+pageNum * +size)
+        .limit(+size)
+        .toArray();
+
+      const count = await productImage.estimatedDocumentCount();
+      res.send({ result, count });
     });
 
     //using PUT method to update quantity at inventory page
     app.put("/inventory/:id", async (req, res) => {
       const id = req.params.id;
       const upDoc = req.body;
-      console.log(id);
-      console.log(upDoc);
+
       const filter = { _id: ObjectId(id) };
       const options = { upsert: true };
       const updateDoc = {
@@ -69,17 +78,34 @@ async function run() {
     //using POST method to add new product here
     app.post("/inventory", async (req, res) => {
       const addedProduct = req.body;
-      const result = await productImage.insertOne(addedProduct);
-      res.send(result);
+      const token = req.headers.authorization;
+      const [email, accessToken] = token.split(" ");
+      const decoded = verifyToken(accessToken);
+
+      if (decoded.email === email) {
+        const result = await productImage.insertOne(addedProduct);
+        res.send(result);
+      } else {
+        res.send({ message: "Unauthorized Access" });
+      }
     });
 
     //getting my items only by email & GET method
     app.get("/myItems", async (req, res) => {
+      const token = req.headers.authorization;
+      const [tokenEmail, accessToken] = token.split(" ");
       const { email } = req.query;
-      const query = { email: email };
-      const cursor = productImage.find(query);
-      const result = await cursor.toArray();
-      res.send(result);
+
+      const decoded = verifyToken(accessToken);
+
+      if (decoded.email === tokenEmail) {
+        const query = { email: email };
+        const cursor = productImage.find(query);
+        const result = await cursor.toArray();
+        res.send({ result, success: true });
+      } else {
+        res.send({ success: false, message: "Unauthorized Access can't show products" });
+      }
     });
 
     //Deleting single my Items by DElETE method
@@ -89,11 +115,33 @@ async function run() {
       const result = await productImage.deleteOne(query);
       res.send(result);
     });
+
+    //implementing basic jwt here and creating token
+    app.post("/createToken", async (req, res) => {
+      const email = req.body;
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET);
+      res.send(token);
+    });
   } finally {
     console.log("Connected to db");
   }
 }
 run().catch(console.dir);
+
+//verify the token by function
+function verifyToken(token) {
+  let email;
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      email = "The email you're trying with is invalid";
+    }
+    if (decoded) {
+      email = decoded;
+    }
+  });
+
+  return email;
+}
 
 app.get("/", (req, res) => {
   res.send("The heroku port is running successfully");
